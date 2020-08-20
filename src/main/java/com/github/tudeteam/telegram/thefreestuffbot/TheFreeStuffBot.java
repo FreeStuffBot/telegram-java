20d4e28a76210c5a7f62f6c85e711d9ef87fe8e3
Rami Sabbagh <ramilego4game@gmail.com>
2020-08-21 00:35:31 +0300

    Implement a basic ping pong bot for testing the framework.

--- src/main/java/com/github/tudeteam/telegram/thefreestuffbot/TheFreeStuffBot.java
+++ src/main/java/com/github/tudeteam/telegram/thefreestuffbot/TheFreeStuffBot.java
@@ -1,7 +1,20 @@
 package com.github.tudeteam.telegram.thefreestuffbot;
 
+import com.github.tudeteam.telegram.thefreestuffbot.framework.commands.Command;
+import com.github.tudeteam.telegram.thefreestuffbot.framework.commands.CommandsHandler;
+import com.github.tudeteam.telegram.thefreestuffbot.framework.commands.Locality;
+import com.github.tudeteam.telegram.thefreestuffbot.framework.commands.Privacy;
+import com.github.tudeteam.telegram.thefreestuffbot.framework.commands.authorizers.Authorizer;
+import com.github.tudeteam.telegram.thefreestuffbot.framework.commands.authorizers.BasicAuthorizer;
+import com.github.tudeteam.telegram.thefreestuffbot.framework.pipes.ConsumeOncePipe;
+import com.github.tudeteam.telegram.thefreestuffbot.framework.pipes.Pipe;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.groupadministration.GetChatMember;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.ChatMember;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class TheFreeStuffBot extends TelegramLongPollingBot {
 
@@ -12,6 +25,48 @@ public class TheFreeStuffBot extends TelegramLongPollingBot {
     protected static final String botDatabaseURI = System.getenv("BOT_DATABASE");
     /* End of configuration */
 
+    protected static final Pipe<Update> updatesPipe = new ConsumeOncePipe<>();
+
+    public TheFreeStuffBot() {
+        Authorizer authorizer = new BasicAuthorizer() {
+            @Override
+            public boolean isAdmin(Message message) {
+                return false;
+            }
+
+            @Override
+            public boolean isOwner(Message message) {
+                return false;
+            }
+
+            @Override
+            public boolean isGroupAdmin(Message message) {
+                if (message.isUserMessage()) return true;
+                try {
+                    ChatMember member = execute(new GetChatMember()
+                            .setChatId(message.getChatId())
+                            .setUserId(message.getFrom().getId()));
+                    return member.getStatus().equals("administrator") || member.getStatus().equals("creator");
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+        };
+
+        CommandsHandler commandsHandler = new CommandsHandler(authorizer, this);
+        updatesPipe.registerHandler(commandsHandler);
+
+        Command pingCommand = new Command("ping", "Pong!", Locality.ALL, Privacy.PUBLIC) {
+            @Override
+            public void action(Message source) throws TelegramApiException {
+                execute(new SendMessage().setChatId(source.getChatId()).setText("Pong 🏓"));
+            }
+        };
+
+        commandsHandler.registerCommand(pingCommand);
+    }
+
     /**
      * This method is called when receiving updates via GetUpdates method
      *
@@ -19,7 +74,8 @@ public class TheFreeStuffBot extends TelegramLongPollingBot {
      */
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println("Received update: " + update);
+        System.out.println(update);
+        System.out.println(updatesPipe.process(update) ? "The update got consumed!" : "The update was not consumed!");
     }
 
     /**

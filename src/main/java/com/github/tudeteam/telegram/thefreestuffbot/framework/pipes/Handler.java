a8da4ffd5a6d9745aa8cc059467ea3d1582cf803
Rami Sabbagh <ramilego4game@gmail.com>
2020-08-20 17:27:01 +0300

    Add a note that the `Handler` is a functional interface.

--- src/main/java/com/github/tudeteam/telegram/thefreestuffbot/framework/pipes/Handler.java
+++ src/main/java/com/github/tudeteam/telegram/thefreestuffbot/framework/pipes/Handler.java
@@ -2,6 +2,8 @@ package com.github.tudeteam.telegram.thefreestuffbot.framework.pipes;
 
 /**
  * An events handler, which would process the events passed to it, and it if it consumed them.
+ * <p>
+ * The handler is accepted as a functional interface and so it can be used with the lambda expression.
  *
  * @param <T> The event type, ex: {@code Update}.
  */
@@ -10,8 +12,7 @@ public interface Handler<T> {
      * Process an event.
      *
      * @param event the event to process.
-     * @return {@code true} if event was consumed,
-     * otherwise {@code false}
+     * @return {@code true} if event was consumed, otherwise {@code false}
      */
     boolean process(T event);
 }

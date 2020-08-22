013c5301cedfd850b1ff42c3dafef01ff77170ce
Rami Sabbagh <ramilego4game@gmail.com>
2020-08-22 23:33:43 +0300

    Wrote 2 classes for fixing the 'until' timestamps.

---
+++ src/main/java/com/github/tudeteam/telegram/thefreestuffbot/database_patch/ConvertUntilIntoString.java
@@ -0,0 +1,30 @@
+package com.github.tudeteam.telegram.thefreestuffbot.database_patch;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.type;
+import static com.mongodb.client.model.Updates.set;
+import static org.bson.BsonType.INT32;
+
+public class ConvertUntilIntoString {
+    public static void main(String[] args) {
+        MongoClient mongoClient = MongoClients.create(System.getenv("database"));
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("freestuffbot");
+        MongoCollection<Document> collection = mongoDatabase.getCollection("games");
+
+        collection.find(type("info.until", INT32)).forEach(document -> {
+            System.out.println("Updating " + document.get("_id"));
+            Document info = (Document) document.get("info");
+            info.put("until", info.getInteger("until").toString());
+
+            collection.updateOne(eq("_id", document.get("_id")), set("info", info));
+        });
+
+        System.out.println("Made all 'until' timestamps into Strings.");
+    }
+}
---
+++ src/main/java/com/github/tudeteam/telegram/thefreestuffbot/database_patch/ConvertUntilIntoTimestamp.java
@@ -0,0 +1,34 @@
+package com.github.tudeteam.telegram.thefreestuffbot.database_patch;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.type;
+import static com.mongodb.client.model.Updates.set;
+import static org.bson.BsonType.STRING;
+
+public class ConvertUntilIntoTimestamp {
+    public static void main(String[] args) {
+        MongoClient mongoClient = MongoClients.create(System.getenv("database"));
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("freestuffbot");
+        MongoCollection<Document> collection = mongoDatabase.getCollection("games");
+
+        collection.find(type("info.until", STRING)).forEach(document -> {
+            System.out.println("Updating " + document.get("_id"));
+
+            int published = document.getInteger("published");
+
+            Document info = (Document) document.get("info");
+            int until = published + Integer.parseInt(info.getString("until")) * 3600 * 24;
+            info.put("until", until);
+
+            collection.updateOne(eq("_id", document.get("_id")), set("info", info));
+        });
+
+        System.out.println("Made all 'until' strings into timestamps.");
+    }
+}

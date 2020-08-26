2f2f18a5c375e0b49285c29cee8b79dbce79e4be
Rami Sabbagh <ramilego4game@gmail.com>
2020-08-26 19:27:51 +0300

    The `BasicAuthorizer` now extends 'LocalityAuthorizer' and 'PrivacyAuthorizer' and works as a combination of them.

--- src/main/java/com/github/tudeteam/telegram/thefreestuffbot/framework/commands/authorizers/BasicAuthorizer.java
+++ src/main/java/com/github/tudeteam/telegram/thefreestuffbot/framework/commands/authorizers/BasicAuthorizer.java
@@ -2,60 +2,18 @@ package com.github.tudeteam.telegram.thefreestuffbot.framework.commands.authoriz
 
 import com.github.tudeteam.telegram.thefreestuffbot.framework.commands.Command;
 import com.github.tudeteam.telegram.thefreestuffbot.framework.commands.ParsedCommand;
-import org.telegram.telegrambots.meta.api.objects.Message;
 
 /**
- * The basic authorizer base.
+ * Determines if a command can be executed according to its' locality and privacy levels.
  */
-public interface BasicAuthorizer extends Authorizer {
-
-    /**
-     * Checks if the message is from an admin of the bot.
-     *
-     * @param message The message to check.
-     * @return {@code true} if it's from a bot admin, {@code false} otherwise.
-     */
-    boolean isAdmin(Message message);
-
-    /**
-     * Checks if the message is from an owner of the bot.
-     *
-     * @param message The message to check.
-     * @return {@code true} if it's from a bot owner, {@code false} otherwise.
-     */
-    boolean isOwner(Message message);
-
-    /**
-     * Checks if the message is from a group admin.
-     *
-     * @param message The message from the user to check.
-     * @return {@code true} if it's from a group admin, {@code false} otherwise.
-     */
-    boolean isGroupAdmin(Message message);
+public interface BasicAuthorizer extends LocalityAuthorizer, PrivacyAuthorizer {
 
     @Override
     default String authorize(ParsedCommand parsedCommand, Command command) {
-        Message source = parsedCommand.origin;
-        switch (command.privacy) {
-            case PUBLIC:
-                return null; //Always authorized for anyone.
-            case GROUP_ADMIN:
-                if (isGroupAdmin(source)) return null;
-            case ADMIN:
-                if (isAdmin(source)) return null;
-            case OWNER:
-                if (isOwner(source)) return null;
-                switch (command.privacy) {
-                    case GROUP_ADMIN:
-                        return "Only group admins are allowed to use this commands ⚠";
-                    case ADMIN:
-                        return "Only the bot staff are allowed to use this command ⚠";
-                    case OWNER:
-                        return "Only the bot owners are allowed to use this command ⚠";
-                }
-            default:
-                new Exception("Unsupported command privacy level: " + command.privacy.name()).printStackTrace();
-                return "A problem has happened while executing the command ⚠";
-        }
+        String rejectionReason = checkLocality(parsedCommand, command);
+        if (rejectionReason != null) return rejectionReason;
+
+        rejectionReason = checkPrivacy(parsedCommand, command);
+        return rejectionReason;
     }
 }

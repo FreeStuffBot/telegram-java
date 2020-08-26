cdcb35c71cf6b6e5a9d7b273d4a4bda0c7245ddd
Rami Sabbagh <ramilego4game@gmail.com>
2020-08-26 14:39:13 +0300

    Javadoc improvements.

--- src/main/java/com/github/tudeteam/telegram/thefreestuffbot/framework/pipes/package-info.java
+++ src/main/java/com/github/tudeteam/telegram/thefreestuffbot/framework/pipes/package-info.java
@@ -1,8 +1,9 @@
 /**
  * A system for processing events, built for Telegram updates.
- * It allows bots to be more modular and object-oriented, by defining a shared interface.
  * <p>
- * The classes defined here can be used with threading, except the {@code register} and {@code unregister}
- * which can result in an undefined behaviour for them, and for ongoing {@code process} calls.
+ * It allows bots to be more modular and object-oriented, by defining a shared interface for updates handlers.
+ * <p>
+ * The classes defined here can be used with threading, except the {@code register} and {@code unregister} methods
+ * which can result in an undefined behaviour for them, and for concurrent {@code process} calls.
  */
 package com.github.tudeteam.telegram.thefreestuffbot.framework.pipes;

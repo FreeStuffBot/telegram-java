8006a0f044ffc97a8d00ee47c9a268210b60a3c3
Rami Sabbagh <ramilego4game@gmail.com>
2020-09-12 20:23:26 +0300

    Implement proper store names.

--- src/main/java/com/github/tudeteam/telegram/thefreestuffbot/structures/Store.java
+++ src/main/java/com/github/tudeteam/telegram/thefreestuffbot/structures/Store.java
@@ -32,5 +32,41 @@ public enum Store {
     @SerializedName("xbox")
     XBOX,
     @SerializedName("other")
-    OTHER
+    OTHER;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case STEAM:
+                return "Steam";
+            case EPIC:
+                return "Epic Games Store";
+            case HUMBLE:
+                return "Humble Bundle";
+            case GOG:
+                return "GOG.com";
+            case UPLAY:
+                return "Uplay";
+            case TWITCH:
+                return "Twitch";
+            case ITCH:
+                return "itch.io";
+            case DISCORD:
+                return "Discord";
+            case APPLE:
+                return "Apple App Store";
+            case GOOGLE:
+                return "Google Play";
+            case SWITCH:
+                return "Nintendo Switch Store";
+            case PS:
+                return "Play Station";
+            case XBOX:
+                return "Xbox";
+            case OTHER:
+                return "Other";
+            default:
+                return name();
+        }
+    }
 }

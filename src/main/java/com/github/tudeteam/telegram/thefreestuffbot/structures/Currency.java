5c65a71741a12585c1322b377db0e8f80375956a
Rami Sabbagh <ramilego4game@gmail.com>
2020-09-25 12:31:13 +0300

    Add the symbol strings to the currency enum.

--- src/main/java/com/github/tudeteam/telegram/thefreestuffbot/structures/Currency.java
+++ src/main/java/com/github/tudeteam/telegram/thefreestuffbot/structures/Currency.java
@@ -4,6 +4,21 @@ package com.github.tudeteam.telegram.thefreestuffbot.structures;
  * Represents the type of currency to display in announcements.
  */
 public enum Currency {
-    USD,
-    EUR
+    USD("$"),
+    EUR("€");
+
+    private final String symbol;
+
+    Currency(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String symbol() {
+        return symbol;
+    }
+
+    @Override
+    public String toString() {
+        return symbol;
+    }
 }

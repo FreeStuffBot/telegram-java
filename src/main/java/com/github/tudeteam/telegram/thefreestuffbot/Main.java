9ad4e8a5b781dcd80e36bb98a79225eda76c9a50
Rami Sabbagh <ramilego4game@gmail.com>
2020-09-27 18:19:57 +0300

    Add SLF4J logging to the Main class.

--- src/main/java/com/github/tudeteam/telegram/thefreestuffbot/Main.java
+++ src/main/java/com/github/tudeteam/telegram/thefreestuffbot/Main.java
@@ -1,10 +1,14 @@
 package com.github.tudeteam.telegram.thefreestuffbot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 public class Main {
+    public static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi botsApi = new TelegramBotsApi();
@@ -12,7 +16,7 @@ public class Main {
         try {
             botsApi.registerBot(new TheFreeStuffBot());
         } catch (TelegramApiRequestException e) {
-            e.printStackTrace();
+            logger.error("Fatal bot crash", e);
         }
     }
 }
